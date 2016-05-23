833c3a3eb3025873f3ea6054b306a3da6913bd37
JamesWangBJ <Jm@wangjiamindeMBP.lan>
2016-05-23 15:33:57 +0800

    测试
    简单测试

---
+++ MyPingGuo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MyPingGuo
+//
+//  Created by 王佳敏 on 16/1/14.
+//  Copyright © 2016年 Jm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *lsdgjldgjlk;
+
+
+@end
+
---
+++ MyPingGuo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  MyPingGuo
+//
+//  Created by 王佳敏 on 16/1/14.
+//  Copyright © 2016年 Jm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+    
+    self.lsdgjldgjlk.constant = 100;
+    
+    // Dispose of any resources that can be recreated.
+}
+
+@end
